31286bf345fe8572631e6b5ebb129d108d156b5a
Krishna Santosh <88792557+Santosh1333@users.noreply.github.com>
2023-07-24 16:32:27 +0530

    Add files via upload

---
+++ Successrrate.py
@@ -0,0 +1,12 @@
+import pickle
+import streamlit as st
+pickle_in=open('model.pkl','rb')
+model=pickle.load(pickle_in)
+e=st.number_input('ENTER EXPERIENCE')
+if st.button('PREDICT'):
+  r=model.predict([[e]]).squeeze()
+  st.success(f'rate: {r}')
+
+
+
+
