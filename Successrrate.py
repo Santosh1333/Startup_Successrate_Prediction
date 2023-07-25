22173c810a1e5fc5825e089f22543b740b132c09
Krishna Santosh <88792557+Santosh1333@users.noreply.github.com>
2023-07-25 13:38:32 +0530

    Successrrate.py

--- Successrrate.py
+++ Successrrate.py
@@ -1,11 +1,25 @@
 import pickle
 import streamlit as st
-pickle_in=open('model.pkl','rb')
-model=pickle.load(pickle_in)
-e=st.number_input('ENTER EXPERIENCE')
+
+try:
+    pickle_in = open('model.pkl', 'rb')
+    model = pickle.load(pickle_in)
+except FileNotFoundError:
+    st.error("Model file not found. Please make sure 'model.pkl' is present.")
+    st.stop()
+except Exception as e:
+    st.error(f"An error occurred while loading the model: {e}")
+    st.stop()
+
+e = st.number_input('ENTER EXPERIENCE')
 if st.button('PREDICT'):
-  r=model.predict([[e]]).squeeze()
-  st.success(f'rate: {r}')
+    try:
+        r = model.predict([[e]]).squeeze()
+        st.success(f'rate: {r}')
+    except AttributeError:
+        st.error("The model does not have a 'predict' method.")
+    except Exception as e:
+        st.error(f"An error occurred while making the prediction: {e}")
 
 
 
